ff6cf518802fdca265817a2b09cc65aebc3655b3
DevYah <yahiaelgamal@gmail.com>
2013-04-07 23:43:53 +0200

    better structure

--- lib/louvian.rb
+++ lib/louvian.rb
@@ -131,44 +131,6 @@ module Louvian
     return node_to_comms_links
   end
 
-  class Community
-    attr_accessor :in, :tot, :nodes_ids, :id
-    @@count = 0
-    def initialize node_id
-      @id = @@count
-      @@count+=1
-      @nodes_ids = [node_id]
-      @in = 0 # sum of links weights inside the community
-      @tot = Louvian.get_adj(node_id).count # sum of links weights incident to the community
-    end
-
-    def insert node, links_to_comm
-      #puts "\t\tinsert node #{node} to comm #{@id}"
-      @nodes_ids << node
-
-      # what makes sense
-      #@in += links_to_comm
-      #@tot += (Louvian.get_adj(node).count - links_to_comm)
-
-      # Copied from the cpp code
-      @in += 2*links_to_comm
-      @tot += (Louvian.get_adj(node).count)
-    end
-
-    def remove node, links_to_comm
-      #puts "\t\tremove node #{node} to comm #{@id}"
-      @nodes_ids.delete node
-
-      # what makes sense
-      #@in -= links_to_comm
-      #@tot -= (Louvian.get_adj(node).count - links_to_comm)
-
-      # Copied from the cpp code
-      @in -= 2*links_to_comm
-      @tot -= (Louvian.get_adj(node).count)
-    end
-
-  end
 end
 s='0 1
 0 8
